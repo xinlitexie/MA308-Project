7a2a263e46929c2992bffc4914634b847846e408
Yuan <xinlitexie@163.com>
2025-11-21 22:41:24 +0800

    add new packages, find a way to count hot words but still need to delete the common words

--- test+graphProvider.R
+++ test+graphProvider.R
@@ -1,8 +1,9 @@
 library(tidyverse)
-library(lubridate)
 library(ggplot2)
-library(corrplot)
+library(cluster)
+library(lubridate)
 library(naniar)
+library(corrplot)
 
 ###读取数据
 data <- read_csv("网易云音乐歌单分析/data.csv")
@@ -39,29 +40,20 @@ split_identity_semicolon <- function(data, identity_col = "identity") {
 data <- split_identity_semicolon(data, "identity")
 
 ###将所有只由是否组成的数据转变为factor类型
-# 更精确地识别只包含"是"和"否"的列
-convert_strict_binary <- function(data) {
+convert_binary <- function(data) {
   # 遍历每一列
   for(col_name in names(data)) {
-    # 检查列是否为字符型或因子型
     if(is.character(data[[col_name]]) || is.factor(data[[col_name]])) {
-      # 获取唯一值（排除NA）
-      unique_vals <- unique(na.omit(data[[col_name]]))
-      
-      # 严格检查是否只包含"是"和"否"
-      if(length(unique_vals) == 2 && 
-         all(sort(unique_vals) == c("否", "是"))) {
-        
-        # 转换为因子，确保水平顺序为"是","否"
+      unique_vals <- unique(data[[col_name]])
+      if(length(unique_vals) == 2 && all(sort(unique_vals) == c("否", "是"))) {
         data[[col_name]] <- factor(data[[col_name]], levels = c("是", "否"))
-        
         cat("已将列", col_name, "转换为因子类型\n")
       }
     }
   }
   return(data)
 }
-data <- convert_strict_binary(data)
+data <- convert_binary(data)
 
 ###删除处理后的列
 remove_columns_tidy <- function(data, col_names) {
@@ -84,5 +76,50 @@ remove_columns_tidy <- function(data, col_names) {
 delete_col_names = c("identity")
 data <- remove_columns_tidy(data, delete_col_names)
 
+###依照play_count数据，将原本数据集分为三类：low，medium，high，方便之后对其中数据进行分析，得到哪些关键数据的高值/低值使得其进入不同类数据集
+summary(data$play_count)
+data$log_play_count = log(data$play_count)
+kmeans_function <- function(data, col_names, k) {
+  # 检查哪些列存在
+  existing_cols <- col_names[col_names %in% names(data)]
+  missing_cols <- col_names[!col_names %in% names(data)]
+  
+  if(length(missing_cols) > 0) {
+    warning("以下列不存在于数据中: ", paste(missing_cols, collapse = ", "))
+  }
+  
+  if(length(existing_cols) > 0) {
+    ###对数化处理播放量的长尾特性
+    log_col <- log(data[[col_names]] + 1)
+    ###k-聚类算法应用
+    kmeans_result <- kmeans(log_col, centers = k)
+    print(kmeans_result)
+    ###寻找聚类点中心，从而完成划分
+    cluster_centers <- kmeans_result$centers
+    print(cluster_centers)
+    sorted_centers <- sort(cluster_centers)
+    ###分为low，medium和high三类
+    low_cluster <- which(cluster_centers == sorted_centers[1])
+    medium_cluster <- which(cluster_centers == sorted_centers[2])
+    high_cluster <- which(cluster_centers == sorted_centers[3])
+    data$cluster <- kmeans_result$cluster
+    low_play_data <- data[data$cluster == low_cluster, ]
+    medium_play_data <- data[data$cluster == medium_cluster, ]
+    high_play_data <- data[data$cluster == high_cluster, ]
+    write.csv(low_play_data, "low_play_data.csv")
+    write.csv(medium_play_data, "medium_play_data.csv")
+    write.csv(high_play_data, "high_play_data.csv")
+  }
+  
+  return(list(low = low_play_data, medium = medium_play_data, high = high_play_data, kmeans_result = kmeans_result))
+}
+
+result <- kmeans_function(data,"play_count", k = 3)
+###注意到解释方差比例达到了81.3%，故而可以认为这个聚类算法十分有效
+###同时，其按照播放量划分出按照播放量低中高的三个数据包
+
+###根据不同数据量的包来提取热词
+
 ###导出处理后的数据
 write_csv(data, "data_processed.csv")
+
